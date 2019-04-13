9a71731b5fffcf21f7e62d3b52420a4b331d0e98
eastrd <yjia285@student.monash.edu>
2019-04-13 22:42:02 +1000

    Optimized in time complexity, but slower in actual runtime.....

--- GDog/482. License Key Formatting.py
+++ GDog/482. License Key Formatting.py
@@ -7,3 +7,19 @@ class Solution:
             if (i + 1) % K == 0 and i < len(S) - 1:
                 res += "-"
         return res[::-1]
+
+    
+    ###############################################
+    # Optimized Version
+    
+    class Solution:
+    def licenseKeyFormatting(self, S: str, K: int) -> str:
+        S = "".join(S.split("-")).upper()
+        res = ""
+        first_len = len(S) % K
+        
+        for i in range(len(S)):
+            res += S[i]
+            if i < len(S) - 1 and (i + 1 == first_len or (i + 1 - first_len) % K == 0):
+                res += "-"
+        return res
