14600d67d20b9fa5560c5ed6c5b5314b458d7899
eastrd <yjia285@student.monash.edu>
2019-04-16 10:29:16 +1000

    Optimized finding leftmost and rightmost to be tracked with indexes, now beats 75% Python runtimes :))

--- GDog/42. Trapping Rain Water.py
+++ GDog/42. Trapping Rain Water.py
@@ -1,17 +1,31 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         
+        def find_right_max(current_i):
+            r = current_i
+            r_max = height[current_i]
+            # Get max right height possible
+            i = current_i + 1
+            while i < len(height):
+                if height[i] >= r_max:
+                    r_max = height[i]
+                    r = i
+                i += 1
+            return r, r_max
+        
         if len(height) < 2:
             return 0
         
         water = 0
+        l_max = height[0]
         
+        r, r_max = find_right_max(1)
         for i in range(1, len(height) - 1):
             h = height[i]
-            l = i - 1
-            r = i + 1
-            l_max, r_max = max(height[:i]), max(height[i+1:])
-
+            l_max = max(l_max, height[i-1])
+            if i > r:
+                r, r_max = find_right_max(i)
+            
             if l_max > h and r_max > h:
                 water += min(l_max, r_max) - h
         
