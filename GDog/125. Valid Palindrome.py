178ff0e733aefaf1f36d9fad7aa26d023d5d33e1
eastrd <yjia285@student.monash.edu>
2019-04-16 21:52:48 +1000

    Wow 50% - I can just use str.isalnum() instead!!!

--- GDog/125. Valid Palindrome.py
+++ GDog/125. Valid Palindrome.py
@@ -7,11 +7,10 @@ class Solution:
         # Length >= 2
         l, r = 0, len(s) - 1
         while l < r:
-            while not (s[l].lower().isalpha() or s[l].lower().isnumeric()) and l < r:
+            while not s[l].lower().isalnum() and l < r:
                 l += 1
-            while not (s[r].lower().isalpha() or s[r].lower().isnumeric()) and l < r:
+            while not s[r].lower().isalnum() and l < r:
                 r -= 1
-            # print(l, r)
             if s[l].lower() == s[r].lower():
                 l += 1
                 r -= 1
