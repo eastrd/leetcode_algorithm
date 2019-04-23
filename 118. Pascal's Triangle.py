7714418eba80c06f28a702047385185cd5c883ea
eastrd <yjia285@student.monash.edu>
2019-04-23 15:16:18 +1000

    79.4% Recursion Yeeee Booooooi

--- 118. Pascal's Triangle.py
+++ 118. Pascal's Triangle.py
@@ -1,28 +1,22 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        # Number of elements == numRows
-        # Sum == 2 ^ (numRows - 1)
-        # 
-        if not numRows:
-            return []
-        if numRows == 1:
-            return [[1]]
-        res = [
-            [1],
-            [1, 1],
-        ]
-        
-        i = 2
-        while i < numRows:
-            j = 0
-            new_row = [1]
-            # Loop through previous array's index
-            while j < len(res[i-1]):
-                if j == len(res[i-1]) - 1:
-                    new_row.append(1)
-                else:
-                    new_row.append(res[i - 1][j] + res[i - 1][j + 1])
-                j += 1
-            i += 1
-            res.append(new_row)
-        return res
+        self.res = []
+        def recur(row):
+            if row == 0:
+                return []
+            if row == 1:
+                self.res.append([1])
+                return [1]
+            if row == 2:
+                self.res.append([1]) 
+                self.res.append([1, 1])
+                return [1, 1]
+            res = [1]
+            prev_row = recur(row-1)
+            for i in range(len(prev_row)-1):
+                res.append(prev_row[i] + prev_row[i+1])
+            res.append(1)
+            self.res.append(res)
+            return res
+        recur(numRows)
+        return self.res
