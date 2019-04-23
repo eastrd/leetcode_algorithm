435e494486511d639c99b01b19a108c9f09951a0
eastrd <yjia285@student.monash.edu>
2019-04-23 16:20:21 +1000

    Faster runtime: 76ms ---> 48ms

--- tree/104. Maximum Depth of Binary Tree.py
+++ tree/104. Maximum Depth of Binary Tree.py
@@ -1,31 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def find_leaf(self, leaf, depth):
-        if leaf:
-            depth += 1
-            if not leaf.left and not leaf.right:
-                return depth
-            elif not leaf.left:
-                return self.find_leaf(leaf.right, depth)
-            elif not leaf.right:
-                return self.find_leaf(leaf.left, depth)
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        self.rec = 0
+        def traverse(node, depth):
+            if not node:
+                if depth > self.rec:
+                    self.rec = depth
             else:
-                return max(self.find_leaf(leaf.left, depth), self.find_leaf(leaf.right, depth))
-        else:
-            return depth
-    
-    def maxDepth(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if not root:
-            return 0
-        depth = 1
-        return max(self.find_leaf(root.left, depth), self.find_leaf(root.right, depth))
+                traverse(node.left, depth+1)
+                traverse(node.right, depth+1)
+                
+        traverse(root, 0)
+        return self.rec
