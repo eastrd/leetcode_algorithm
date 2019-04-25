e53b7c2d332f5d1792dfd009453f7a33f85c098d
eastrd <yjia285@student.monash.edu>
2019-04-25 19:15:02 +1000

    61.60% Optimized using array to keep track of minimum integer of each score... (So hard to debug)

--- 300. Longest Increasing Subsequence.py
+++ 300. Longest Increasing Subsequence.py
@@ -4,12 +4,15 @@ class Solution:
             return len(nums)
 
         # Length >= 2
-        seq = [0] * len(nums)
-        seq[0] = 1
+        score = [1] * len(nums)
+        minimum = [None, nums[0]]
+        
         for i in range(1, len(nums)):
-            for j in range(0, i):
-                if nums[i] > nums[j] and seq[j] + 1 > seq[i]:
-                    seq[i] = seq[j] + 1
-                if seq[i] == 0:
-                    seq[i] = 1
-        return max(seq)
+            for j in range(1, len(minimum)):
+                if nums[i] > minimum[j] and j + 1 > score[i]:
+                    score[i] = j + 1
+                    if score[i] > len(minimum) - 1:
+                        minimum.append(nums[i])
+            if nums[i] < minimum[score[i]]:
+                minimum[score[i]] = nums[i]
+        return len(minimum) - 1
