c2d62d91991782bbbc7452d2f655cfacd6c84999
eastrd <yjia285@student.monash.edu>
2019-04-26 16:30:57 +1000

    92.06% But I had to refer to soln. My code was mostly right but minor boundary mistakes.... What a shame!

---
+++ 19. Remove Nth Node From End of List.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        if not head.next or not head:
+            return []
+        
+        fast = slow = head
+        i = 0
+        while i != n:
+            fast = fast.next
+            i += 1
+        
+        if not fast:
+            return head.next
+        
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        
+        slow.next = slow.next.next
+        
+        return head
